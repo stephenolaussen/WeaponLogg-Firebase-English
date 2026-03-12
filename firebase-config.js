634ece722cc2574fecc75069990e0d6e3b7d9e96
Stephenolaussen <Stephenolaussen@gmail.com>
2026-03-12 20:03:05 +0100

    firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase
 
 // Firebase configuration - PUT IN YOUR CONFIG HERE!
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBVIHNdnmY_39KXY3PKlaJ88DotXS_D934",
+  authDomain: "weaponlog-private.firebaseapp.com",
+  projectId: "weaponlog-private",
+  storageBucket: "weaponlog-private.firebasestorage.app",
+  messagingSenderId: "601806962636",
+  appId: "1:601806962636:web:23d9ad83dd203a848de769",
+  measurementId: "G-J1TDB4CE21"
 };
 
 const app = initializeApp(firebaseConfig);
